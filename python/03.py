79452a1041b39658bf679074c646ba6173a58b6d
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-03 06:52:38 +0100

    [day03] clean up and convert to oneliners

--- python/03.py
+++ python/03.py
@@ -1,35 +1,21 @@
+# Day 3: Rucksack Reorganization
+
 from santas_little_helpers.helpers import *
 
 data = get_input('inputs/03.txt')
 
-def item_priority(item):
-    if item.islower():
-        return ord(item) - 96
-    else:
-        return ord(item) - 38
+item_priority = lambda x: ord(x) - 96 if x.islower() else ord(x) - 38
 
-party_1 = 0
-for line in data:
-    rl = len(line) // 2
-    left = line[:rl]
-    right = line[rl:]
-    #print(set(left) & set(right))
-    for c in set(left) & set(right):
-        #print(item_priority(c))
-        party_1 += item_priority(c)
 
-print(party_1)
+party_1 = sum((sum (item_priority(c) for c in set(line[:len(line)//2]) & set(line[len(line)//2:]))
+                for line in data))
 
-party_2 = 0
-for n, first in enumerate(data[::3]):
-    third = data[n*3+2]
-    second = data[n*3+1]
-    #print('first in tge group', first, second, third)
-    #print(set(first) & set(second) & set(third))
-    party_2 += sum(item_priority(c) for c in set(first) & set(second) & set(third))
-#print(item_priority('A'))
-# 3875 incorrect
-print(party_2)
+party_2 = sum(sum(item_priority(c) for c in set(first) & set(second) & set(third))
+                for first, second, third in zip(data[::3], data[1::3], data[2::3]))
+
+print_solutions(party_1, party_2)
+
+
 
 def test_one():
     assert party_1 == 8243
