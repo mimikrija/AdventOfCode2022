366fd6cc84d2be81d399e255eaf62cc5e2cb765a
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-04 07:11:12 +0100

    [day04] limits approach similar to 2021 day 22

--- python/04.py
+++ python/04.py
@@ -1,20 +1,15 @@
 from santas_little_helpers.helpers import *
 from re import findall
 
-data = get_input('inputs/04.txt')
+data = [tuple(map(int, findall(r'\d+', line))) for line in get_input('inputs/04.txt')]
 
-party_1 = 0
-party_2 = 0
-for sec in data:
-    a, b = map(int, findall(r'\d+', sec.split(',')[0]))
-    c, d = map(int, findall(r'\d+', sec.split(',')[1]))
-    first = set(list(range(a, b+1)))
-    second = set(list(range(c, d+1)))
-    #print(a, b, first, second)
-    if second & first == second or first & second == first:
-        party_1 += 1
-    if len(second&first) > 0:
-        party_2 += 1
+
+overlap = lambda l1, r1, l2, r2: not (r1 < l2 or l1 > r2)
+full_overlap = lambda l1, r1, l2, r2: l2 <= l1 <= r1 <= r2 or l1 <= l2 <= r2 <= r1
+
+
+party_1, party_2 = (sum(func(*sections) for sections in data) for func in (full_overlap, overlap))
+
 
 print_solutions(party_1, party_2)
 
