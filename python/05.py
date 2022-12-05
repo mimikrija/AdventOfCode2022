7ded6e4227c08848dbbbf77e7162c5c935dc0434
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-05 06:21:04 +0100

    [day05] first go - I didn't bother parsing stacks
    I just rewrote them manually :shrug:

---
+++ python/05.py
@@ -0,0 +1,34 @@
+from collections import deque
+from re import findall
+
+from santas_little_helpers.helpers import *
+
+data = get_input('inputs/05.txt')
+
+operations = ((map(int, findall(r'\d+', line))) for line in data[10:])
+
+
+
+crates = {
+    1: deque('RGHQSBTN'),
+    2: deque('HSFDPZJ'),
+    3: deque('ZHV'),
+    4: deque('MZJFGH'),
+    5: deque('TZCDLMSR'),
+    6: deque('MTWVHZJ'),
+    7: deque('TFPLZ'),
+    8: deque('QVWS'),
+    9: deque('WHLMTDNC')
+}
+
+for qt, origin, destination in operations:
+    buffer = deque([])
+    for _ in range(qt):
+        buffer.appendleft(crates[origin].pop())
+    for _ in range(qt):
+        crates[destination].append(buffer.popleft())
+
+for c, p in crates.items():
+    print(c, p.pop())
+
+# PTWLTDSJV
