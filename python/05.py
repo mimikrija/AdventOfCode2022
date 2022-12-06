3ac61798078df8b6209794efe468f3363cd25966
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-06 09:40:27 +0100

    [day05] really no need for deque here

--- python/05.py
+++ python/05.py
@@ -1,6 +1,6 @@
 # Day 5: Supply Stacks
 
-from collections import defaultdict, deque
+from collections import defaultdict
 from copy import deepcopy
 from re import findall
 
@@ -10,7 +10,7 @@ data = get_input('inputs/05.txt')
 operations = [tuple(map(int, findall(r'\d+', line))) for line in data[10:]]
 
 # parse stacks
-stacks = defaultdict(deque)
+stacks = defaultdict(list)
 for line in data[7::-1]:
     for num, crate in enumerate(line[1::4], 1):
         if crate != ' ':
@@ -20,13 +20,10 @@ for line in data[7::-1]:
 def solve(in_stacks, operations, part2=False):
     stacks = deepcopy(in_stacks)
     for qt, origin, destination in operations:
-        buffer = deque([])
-        for _ in range(qt):
-            if not part2:
-                stacks[destination].append(stacks[origin].pop())
-            else:
-                buffer.appendleft(stacks[origin].pop())
-        stacks[destination] += buffer
+        if not part2:
+            stacks[destination] += [stacks[origin].pop() for _ in range(qt)]
+        else:
+            stacks[destination] += reversed([stacks[origin].pop() for _ in range(qt)])
     return ''.join(stack.pop() for stack in stacks.values())
 
 
