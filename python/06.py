531011511442d2fd53f59368f450f4e2ac39fbcc
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-06 13:16:08 +0100

    [day06] fix whitespaces

--- python/06.py
+++ python/06.py
@@ -5,13 +5,13 @@ from santas_little_helpers.helpers import *
 def find_marker(in_string, n):
     for pos in range(len(in_string)):
         if len(set(in_string[pos:pos+n])) == n:
-            return pos + n
+            return pos+n
 
 
 
 data = get_input('inputs/06.txt')[0]
 
-party_1, party_2 =(find_marker(data, n) for n in (4, 14))
+party_1, party_2 = (find_marker(data, n) for n in (4, 14))
 
 print_solutions(party_1, party_2)
 
