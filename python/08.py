4a8a679da797eb335488b9f21a3fb3b8fe1a905c
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-08 10:10:22 +0100

    [day08] simplify a bit

--- python/08.py
+++ python/08.py
@@ -2,6 +2,11 @@
 
 from santas_little_helpers.helpers import *
 
+data = get_input('inputs/08.txt')
+TREE_HEIGHTS = {(x, y): int(num) for y, row in enumerate(data) for x, num in enumerate(row)}
+MAX_X = len(data[0])
+MAX_Y = len(data)
+
 
 def is_visible(location, grid):
     xt, yt = location
@@ -15,18 +20,12 @@ def scenic_score(location, grid):
     xt, yt = location
     height = grid[location]
 
-    look_left = next((n for n, x in enumerate(range(xt-1, -1, -1), 1) if grid[(x, yt)] >= height), xt)
-    look_right = next((n for n, x in enumerate(range(xt+1, MAX_X),1) if grid[(x, yt)] >= height), MAX_X-1-xt)
-    look_up = next((n for n, y in enumerate(range(yt-1, -1, -1),1) if grid[(xt, y)] >= height), yt)
-    look_down = next((n for n, y in enumerate(range(yt+1, MAX_Y),1) if grid[(xt, y)] >= height), MAX_Y-1-yt)
-
-    return look_left * look_right * look_up * look_down
+    return next((n for n, x in enumerate(range(xt-1, -1, -1), 1) if grid[(x, yt)] >= height), xt) * \
+        next((n for n, x in enumerate(range(xt+1, MAX_X),1) if grid[(x, yt)] >= height), MAX_X-1-xt) * \
+        next((n for n, y in enumerate(range(yt-1, -1, -1),1) if grid[(xt, y)] >= height), yt) * \
+        next((n for n, y in enumerate(range(yt+1, MAX_Y),1) if grid[(xt, y)] >= height), MAX_Y-1-yt)
 
 
-data = get_input('inputs/08.txt')
-TREE_HEIGHTS = {(x, y): int(num) for y, row in enumerate(data) for x, num in enumerate(row)}
-MAX_X = len(data[0])
-MAX_Y = len(data)
 
 party_1 = sum(is_visible(tree, TREE_HEIGHTS) for tree in TREE_HEIGHTS.keys())
 party_2 = max(scenic_score(tree, TREE_HEIGHTS) for tree in TREE_HEIGHTS.keys())
