7f538dd41ba97a48e0d709e0513fcfca6fd99408
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-08 13:16:00 +0100

    [day08] is this functional programming?

--- python/08.py
+++ python/08.py
@@ -11,33 +11,29 @@ MAX_X = len(data[0])
 MAX_Y = len(data)
 
 
-def is_visible(location, grid):
-    xt, yt = location
-    height = grid[location]
-
-    left = ((x, yt) for x in reversed(range(0, xt)))
-    right = ((x, yt) for x in range(xt+1, MAX_X))
-    up = ((xt, y) for y in reversed(range(0, yt)))
-    down = ((xt, y) for y in range(yt+1, MAX_Y))
-
-    return any((all(grid[pair] < height for pair in side)) for side in (left, right, up, down))
-
-def scenic_score(location, grid):
-    xt, yt = location
-    height = grid[location]
-
+def directions(tree):
+    """returns coordinates to check in a given direction + default solution if all trees are lower than the reference tree for pt2 """
+    xt, yt = tree
     left = (((x, yt) for x in reversed(range(0, xt))), xt)
-    right = (((x, yt) for x in range(xt+1, MAX_X)), MAX_X -1 - xt)
+    right = (((x, yt) for x in range(xt+1, MAX_X)), MAX_X-1-xt)
     up = (((xt, y) for y in reversed(range(0, yt))), yt)
     down = (((xt, y) for y in range(yt+1, MAX_Y)), MAX_Y-1-yt)
-
-    return (reduce(mul, (next((n for n, pair in enumerate(left, 1) if grid[pair] >= height), default) for left, default in (left, right, up, down))))
-
+    return (left, right, up, down)
 
 
+def is_visible(location, grid):
+    height = grid[location]
+    return any((all(grid[pair] < height for pair in side)) for side, _ in directions(location))
 
-party_1 = sum(is_visible(tree, TREE_HEIGHTS) for tree in TREE_HEIGHTS.keys())
-party_2 = max(scenic_score(tree, TREE_HEIGHTS) for tree in TREE_HEIGHTS.keys())
+def scenic_score(location, grid):
+    height = grid[location]
+    return (reduce(mul, (next((n for n, pair in enumerate(left, 1) if grid[pair] >= height), default) for left, default in directions(location))))
+
+
+
+
+party_1, party_2 = (what(which(tree, TREE_HEIGHTS) for tree in TREE_HEIGHTS.keys()) for what, which in ((sum, is_visible), (max, scenic_score)))
+
 
 print_solutions(party_1, party_2)
 
