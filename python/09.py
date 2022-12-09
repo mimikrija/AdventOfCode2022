9b5d368363c3800a09cdcec04b5885fa9ec0fed1
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-09 09:58:45 +0100

    [day09] final cleanup

--- python/09.py
+++ python/09.py
@@ -1,12 +1,6 @@
+# Day 9: Rope Bridge
 
 from santas_little_helpers.helpers import *
-from cmath import phase
-from math import sqrt
-
-
-data = get_input('inputs/09.txt')
-
-movements = [(line.split()[0], int(line.split()[1])) for line in data]
 
 MOVE = {
     'R': 1+0j,
@@ -22,68 +16,43 @@ def get_nine(position):
     return {complex(xn, yn) for xn, yn in ((x + dx, y + dy) for dx in (-1, 0, 1) for dy in (-1, 0, 1))}
 
 def is_touching(head, tail):
-    if tail in get_nine(head):
-        return True
-    return False
+    return tail in get_nine(head)
 
+def unit(num):
+    get_unit = lambda x: x/abs(x) if x != 0 else x
+    dx = get_unit(num.real)
+    dy = get_unit(num.imag)
+    return complex(dx, dy)
 
-def sign(num):
-    x = num.real
-    y = num.imag
-
-
-    return complex(x/abs(x) if x != 0 else x, y/abs(y) if y !=0 else y)
-
-def inrow(num1, num2):
-    return num1.real == num2.real
-
-def incolumn(num1, num2):
-    return num1.imag == num2.imag
 
 def move_tail(new_head, old_tail):
     if is_touching(new_head, old_tail):
         return old_tail
+    return old_tail + unit(new_head - old_tail)
+
+
+def move_the_rope(instructions, rope_length):
+    rope = rope_length*[0+0j]
+    tail_positions = {rope[-1]}
+
+    for direction, qt in instructions:
+        for _ in range(1, qt+1):
+            rope[0] += MOVE[direction] # update head poisition
+            for pos in range(1, rope_length):
+                # all knots need to follow the head
+                rope[pos] = move_tail(rope[pos-1], rope[pos])
+            tail_positions.add(rope[rope_length-1])
     
-
-    if (inrow(old_tail, new_head) and abs(old_tail.imag - new_head.imag) == 2) or \
-        (incolumn(old_tail, new_head) and abs(old_tail.real - new_head.real)==2):
-        diff =new_head - old_tail
-        return old_tail + sign(diff)
-    
-    elif not inrow(old_tail, new_head) and not incolumn(old_tail, new_head):
-        # move diagonaly one step
-        diff =new_head - old_tail
-
-        return old_tail + sign(diff)
+    return len(tail_positions)
 
 
-head = 0+0j
-tail = 0+0j
-tail_positions = set()
-for direction, qt in movements:
-    #print(head, tail, qt)
-    for step in range(1, qt+1):
-        head += MOVE[direction]
-        tail = move_tail(head, tail)
-        tail_positions.add(tail)
-
-party_1 = len(tail_positions)
 
 
-head = 0+0j
-tail = 0+0j
-rope = 10*[0+0j]
+data = get_input('inputs/09.txt')
 
-tail_positions = {tail}
-for direction, qt in movements:
-    for step in range(1, qt+1):
-        rope[0] += MOVE[direction]
-        for pos in range(1, 10):
-            # all knots need to follow the head
-            rope[pos] = move_tail(rope[pos-1], rope[pos])
-        tail_positions.add(rope[9])
+instructions = [(line.split()[0], int(line.split()[1])) for line in data]
 
-party_2 = len(tail_positions)
+party_1, party_2 = (move_the_rope(instructions, rope_length) for rope_length in (2, 10))
 
 print_solutions(party_1, party_2)
 
