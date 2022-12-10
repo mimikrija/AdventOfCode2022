79a4fd2ec2de23bfb357a3c7b0fe7b2abe80e968
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-10 19:13:49 +0100

    [day10] both parts with Nico

--- python/10.py
+++ python/10.py
@@ -9,6 +9,7 @@ data = get_input('inputs/10.txt')
 cycle = 0
 x=1
 xses=[]
+screenx = []
 crt = defaultdict(str)
 for line in data:
     ins = line.split()
@@ -21,14 +22,24 @@ for line in data:
                 xses.append(x*cycle)
             if cycle == 220:
                 print (x, x*cycle, line, cycle)
-            crt[cycle//40] += ''
+            screenx.append(x)
         x += int(ins[1])
     else:
         cycle += 1
         if cycle in range(20, 221, 40):
                 print(x, x*cycle, line, cycle)
                 xses.append(x*cycle)
+        screenx.append(x)
 
 print(cycle)
-
+print(len(screenx))
 print(sum(xses))
+
+#print(screenx)
+for pos, x in enumerate(screenx):
+    if pos % 40 == 0:
+        print('')
+    if x-1 <= (pos)%40 <= x+1:
+        print('#', end='')
+    else:
+        print(' ', end='')
