0cdb1f0bf381682d42bea86b71725cb875773308
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-11 10:42:14 +0100

    [day11] pt1 first go with test

--- python/11.py
+++ python/11.py
@@ -1,6 +1,8 @@
 
 from santas_little_helpers.helpers import *
 from collections import deque
+from math import prod
+from primefac import primefac
 
 data = get_input('inputs/11.txt', False, '\n\n')
 data = get_input('inputs/11e.txt', False, '\n\n')
@@ -12,7 +14,6 @@ for chunk in data:
     for n, line in enumerate(lines):
         if n == 0:
             monkey = line[:-1]
-            print(monkey)
         bla = line.split(': ')
         if n == 1:
             items = '[' + bla[1] + ']'
@@ -24,7 +25,7 @@ for chunk in data:
             actiont = int(bla[1][-1])
         if n == 5:
             actionf = int(bla[1][-1])
-    monkeys.append([deque(eval(items)), operation, test, actiont, actionf])
+    monkeys.append([deque(eval(items)), operation, test, actiont, actionf, 0])
 
 
 
@@ -32,22 +33,35 @@ def round(monkeys):
     for num, monkey in enumerate(monkeys):
         while monkey[0]:
             old = monkey[0].popleft()
-            print(f'monkey {num} inspects item of worry level of {old}')
+            monkey[5] += 1
+            
+            #print(f'monkey {num} inspects item of worry level of {old}')
             new = eval(monkey[1])
+
             new //= 3
-            if new % monkey[2]:
+            if new%monkey[2]==0:
                 monkeys[monkey[3]][0].append(new)
             else:
                 monkeys[monkey[4]][0].append(new)
         
 
 
-for _ in range(1):
+
+
+for rou in range(20):
     round(monkeys)
 
 
-for monkey in monkeys:
-    print(len(monkey[0]))
+top_inspectors = sorted((monkey[5] for monkey in monkeys), reverse=True)[:2]
+
+party_1 = top_inspectors[0]*top_inspectors[1]
+
+def test_one():
+    assert party_1 == 54036
+
+
+
+
 
 
 
