a94a8ef9815ddbe85bb9f7a2e91ef19f9ddfb98e
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-12 12:19:49 +0100

    [day12] both parts what is this

--- python/12.py
+++ python/12.py
@@ -2,6 +2,7 @@
 from santas_little_helpers.helpers import *
 
 from collections import deque
+from queue import PriorityQueue
 DELTAS ={
     -1+0j, # left
      1+0j, # right
@@ -11,39 +12,51 @@ DELTAS ={
 
 
 def get_neighbors(location, cave_floor, cave_heights):
-    neighbs = [candidate for candidate in (location + delta for delta in DELTAS) if candidate in cave_floor and cave_heights[candidate] <= cave_heights[location]+1]
-    return sorted(neighbs, key=lambda x: cave_heights[x])
+    return [candidate for candidate in get_four_neighbors(location) if candidate in cave_floor and cave_heights[candidate] <= cave_heights[location]+1]
+
+def get_path_length(came_from, start, end):
+    current = end
+    path = []
+
+    while current != start:
+        path.append(current)
+        try:
+            current = came_from[current]
+        except:
+            return None
+
+    return len(path)
 
 
-
-def traverse_floor(cave_floor, cave_heights, start, end):
-    frontier = deque([start])
-    reached = set({start})
+def traverse_floor(cave_floor, cave_heights, start, end, min_length=None):
+    frontier = PriorityQueue()
+    frontier.put((0, start))
+    cost_so_far = dict()
+    cost_so_far[start] = ord('a')
     came_from = {start: None}
-    while frontier:
-        #print(frontier)
-        current = frontier.pop()
-        current_height = cave_heights[current]
+    while not frontier.empty():
+        _, current = frontier.get()
+        if current == end:
+            return get_path_length(came_from, start, end)
+        if get_path_length(came_from, start, current) > min_length:
+            return
+
         neighbors = get_neighbors(current, cave_floor, cave_heights)
-        #print(neighbors)
 
         for neighbor in neighbors:
-            if neighbor not in came_from: # and cave_heights[neighbor] <= current_height+1:
+            #print(neighbor)
+            height = cave_heights[current]
+            new_cost = cost_so_far[current] + height
+            if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]: # and cave_heights[neighbor] <= current_height+1:
                 #print(f'curent {current}, neighbor: {neighbor}')
-                frontier.append(neighbor)
-                reached.add(neighbor)
+                cost_so_far[neighbor] = new_cost
+                frontier.put((new_cost, neighbor))
+                #priority = current_height
                 came_from[neighbor] = current
 
 
+    return get_path_length(came_from, start, end)
 
-    current = end
-    path = []
-    while current != start:
-        path.append(current)
-        current = came_from[current]
-    #path.append(start)
-    return path
-    #return reached
 
 
 
@@ -54,7 +67,7 @@ cave_floor = []
 cave_heights = dict()
 for row, line in enumerate(lines):
     for col, height in enumerate(line):
-        position = complex(col, row)
+        position = (col, row)
         cave_floor.append(position)
         if height == 'E':
             cave_heights[position] = ord('z')
@@ -66,8 +79,21 @@ for row, line in enumerate(lines):
             cave_heights[position] = ord(height)
 
 print(START, END)
+all_cisit = []
+current_min = len(cave_heights)
 
-visited = traverse_floor(cave_floor, cave_heights, START, END)
-print(visited)
-print(len(visited)) # not 509, 512, 589
+for pos, heig in cave_heights.items():
+    if heig == ord('a'):
+        #print(pos)
+        bla = traverse_floor(cave_heights.keys(), cave_heights, pos, END, current_min)
+        if bla is not None:
+            current_min = new_min if (new_min:=bla) < current_min else current_min
+
+print(current_min)
+
+
+
+#visited = traverse_floor(cave_floor, cave_heights, START, END)
+
+#print(len(visited)) # not 509, 512, 589 not 443
 
