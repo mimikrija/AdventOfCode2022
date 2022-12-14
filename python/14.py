539f7c20f99af52cc00a86c28b46faa90d560f5c
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-14 09:47:26 +0100

    [day14] both parts, messy

--- python/14.py
+++ python/14.py
@@ -1,9 +1,9 @@
 
 from santas_little_helpers.helpers import *
 
-#data = get_input('inputs/14.txt')
-data = get_input('inputs/14e.txt')
-
+data = get_input('inputs/14.txt')
+#data = get_input('inputs/14e.txt')
+sign = lambda x: 1 if x >= 0 else -1
 
 def generate_cave(data):
     rocks = set()
@@ -14,8 +14,8 @@ def generate_cave(data):
             #     print(first, second, 'it is a diagonal')
             horiz_delta = second[0] - first[0]
             vert_delta = second[1] - first[1]
-            rocks |= {complex(first[0] + x, first[1]) for x in range(horiz_delta)}
-            rocks |= {complex(first[0], first[1]+y) for y in range(vert_delta)}
+            rocks |= {complex(first[0] + sign(horiz_delta)*x, first[1]) for x in range(sign(horiz_delta)*horiz_delta)}
+            rocks |= {complex(first[0], first[1]+sign(vert_delta)*y) for y in range(sign(vert_delta)*vert_delta)}
             rocks.add(complex(*second))
     return rocks
 
@@ -27,10 +27,8 @@ def possible_directions(location):
 def one_sand(rocks, sand, bottom_line, start=500+0j):
     current = start
     while True:
-        print(current)
         candidates = [pos for pos in possible_directions(current) if pos not in sand and pos not in rocks]
         if len(candidates) == 0:
-            print('cur', current)
             return current # the particle stopped
         
         else:
@@ -39,11 +37,28 @@ def one_sand(rocks, sand, bottom_line, start=500+0j):
             return
 
 
-
-bottom_line = max(generate_cave(data), key=lambda x:x.imag)
-
 rocks = generate_cave(data)
+
+bottom_line = max(rocks, key=lambda x:x.imag) + 0+2j
+print(bottom_line)
+for x in range(3*int(-bottom_line.imag), 2*int(bottom_line.imag+1), 1):
+    rocks.add(bottom_line+complex(x, 0))
+
+
 sand = set()
 
-sand.add(one_sand(rocks, sand, bottom_line))
-print(sand)
+#sand.add(one_sand(rocks, sand, bottom_line+2))
+#party_1 = 0
+while True:
+    sand_particle = one_sand(rocks, sand, bottom_line)
+    if sand_particle:
+        sand.add(sand_particle)
+        if sand_particle == 500+0j:
+            break
+    else:
+        break
+
+party_1 = len(sand)
+print(party_1)
+
+# 14276 too low
