2774f3b1fec966f26e3ff5fde9d30caa727acdc9
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-15 08:40:26 +0100

    [day15] pt1 - I added the condition that radius must be smaller than vertical distance!

--- python/15.py
+++ python/15.py
@@ -3,25 +3,22 @@ from santas_little_helpers.helpers import *
 from re import findall
 
 data = get_input('inputs/15.txt')
-data = get_input('inputs/15e.txt')
+#data = get_input('inputs/15e.txt')
 
 def manhattan(one, two=0+0j):
-    dist = one - two
-    return int(abs(dist.real) + abs(dist.imag))
+    radius = one - two
+    return int(abs(radius.real) + abs(radius.imag))
 
 datad = []
-all_x = []
+
 for line in data:
     numbers = list(map(int, findall(r'-?\d+',line)))
-    all_x.append(numbers[0])
-    all_x.append(numbers[2])
+
     sensor = complex(numbers[0], numbers[1])
     beacon = complex(numbers[2], numbers[3])
     datad.append((sensor, beacon))
 
-# MIN_X = min(all_x)
-# MAX_X = max(all_x)
-# print(MAX_X-MIN_X)
+
 
 distances = []
 for sensor, beacon in datad:
@@ -30,27 +27,32 @@ for sensor, beacon in datad:
 
 
 CHECK_Y = 2000000
-CHECK_Y = 10
+#CHECK_Y = 10
 taken = set()
 for sensor, beacon in datad:
     if sensor.imag == CHECK_Y:
-        taken.add(int(sensor.imag))
+        taken.add(int(sensor.real))
     if beacon.imag == CHECK_Y:
-        taken.add(int(beacon.imag))
+        taken.add(int(beacon.real))
+
+
 
 total = set()
-for cent, dist in distances:
+for cent, radius in distances:
     vert = int(abs(cent.imag - CHECK_Y))
-    hor = int(abs(dist - vert))
-    #print(cent, dist, 'hor ', hor, 'vert', vert)
-    r = set(range(int(cent.real-hor), int(cent.real + hor+1)))
-    print(cent, sorted(r))
+    if radius < vert:
+        continue
+    #print(vert)
+    hor = int(abs(radius - vert))
+    #print(cent, radius, 'hor ', hor, 'vert', vert)
+    r = set(range(int(cent.real-hor), int(cent.real + hor)+1))
+    #print(cent, sorted(r), radius, hor, vert)
     total |= r
-
+print(taken)
 total -= taken
 
 
-print(sorted(total))
+#print(sorted(total))
 print(len(total)) # not 5843797
 #                       5843797
 # not 5843798
