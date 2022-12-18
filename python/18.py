a43d6c4397d61983088de31696316f9914db2278
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-18 10:51:09 +0100

    [day18] part 1 with sets and neighbors

---
+++ python/18.py
@@ -0,0 +1,26 @@
+# Day 18: Boiling Boulders
+
+from santas_little_helpers.helpers import *
+
+DELTAS = {(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)}
+
+
+cubes = [tuple(map(int, line.split(','))) for line in get_input('inputs/18.txt')]
+
+
+def cube_neigbors(in_cube):
+    return {add_coordinates(in_cube, delta) for delta in DELTAS}
+
+
+def count_free_sides(cube, cubes):
+    return sum((neighbor not in cubes) for neighbor in cube_neigbors(cube))
+
+
+party_1 = sum(count_free_sides(cube, cubes) for cube in cubes)
+print_solutions(party_1)
+
+
+
+
+def test_one():
+    assert party_1 == 4310
