7c5dc39450ca50e136ec6dd5aa79d8daf733afc7
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-18 11:07:50 +0100

    [day18] define void

--- python/18.py
+++ python/18.py
@@ -1,15 +1,16 @@
 # Day 18: Boiling Boulders
 
 from santas_little_helpers.helpers import *
+from math import prod
 
 DELTAS = {(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)}
 
 
-lava = [tuple(map(int, line.split(','))) for line in get_input('inputs/18.txt')]
+lava = {tuple(map(int, line.split(','))) for line in get_input('inputs/18.txt')}
 
-
-SIZE = 6
-SIZE = 21
+MIN_X, MIN_Y, MIN_Z = (min(lava, key=lambda x:x[n])[n] for n in range(3))
+MAX_X, MAX_Y, MAX_Z = (max(lava, key=lambda x:x[n])[n] for n in range(3))
+SIZE = [MAX-MIN+1 for MAX, MIN in zip((MAX_X, MAX_Y, MAX_Z), (MIN_X, MIN_Y, MIN_Z))]
 
 
 def cube_neigbors(in_cube):
@@ -18,6 +19,10 @@ def cube_neigbors(in_cube):
 
 def count_free_sides(cube, lava):
     return sum((neighbor not in lava) for neighbor in cube_neigbors(cube))
+
+
+void = {cube for x in range(MIN_X, MAX_X+1) for y in range(MIN_Y, MAX_Y+1) for z in range(MIN_Z, MAX_Z+1) if (cube := (x, y, z)) not in lava}
+
 
 
 
