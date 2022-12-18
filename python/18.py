1a3b24023fdcb12729d2718ecdfb1304f0f6b71f
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-18 10:52:55 +0100

    [day18] more meaningful names

--- python/18.py
+++ python/18.py
@@ -5,18 +5,26 @@ from santas_little_helpers.helpers import *
 DELTAS = {(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)}
 
 
-cubes = [tuple(map(int, line.split(','))) for line in get_input('inputs/18.txt')]
+lava = [tuple(map(int, line.split(','))) for line in get_input('inputs/18.txt')]
+
+
+SIZE = 6
+SIZE = 21
 
 
 def cube_neigbors(in_cube):
     return {add_coordinates(in_cube, delta) for delta in DELTAS}
 
 
-def count_free_sides(cube, cubes):
-    return sum((neighbor not in cubes) for neighbor in cube_neigbors(cube))
+def count_free_sides(cube, lava):
+    return sum((neighbor not in lava) for neighbor in cube_neigbors(cube))
 
 
-party_1 = sum(count_free_sides(cube, cubes) for cube in cubes)
+
+
+
+
+party_1 = sum(count_free_sides(cube, lava) for cube in lava)
 print_solutions(party_1)
 
 
