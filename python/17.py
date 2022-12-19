3adc9a5a17409eaaf4d8509d10ee302d6a579695
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-19 17:11:45 +0100

    [day17] give more meaningful variable names

--- python/17.py
+++ python/17.py
@@ -21,44 +21,49 @@ SHAPES = [[(2+col, row) for row, line in enumerate(reversed(shape))
                         for shape in (MINUS, PLUS, L, I, SQUARE)]
 
 
-def update_position(shape, dx, dy):
+def move(shape, dx, dy):
     return [(x+dx, y+dy) for (x, y) in shape]
 
-def touched(shape, taken):
-    return any(point in taken for point in shape)
+def penetrating_floor(shape, tower):
+    return any(point in tower for point in shape)
 
-def highest_point(taken):
-    return max(taken, key=lambda x:x[1])[1]
+def penetrating_wall(shape):
+    return any(x < 0 or x > 6 for (x, y) in shape)
+
+def highest_point(tower):
+    return max(tower, key=lambda x:x[1])[1]
 
 
-def play_tetris(num_of_rocks, hot_air, taken={(x, 0) for x in range(7)}):
+def drop_rocks(num_of_rocks, hot_air, tower={(x, 0) for x in range(7)}):
     num_h = 0
     for num_r in range(num_of_rocks):
-        height = highest_point(taken)
-        current_shape = update_position(SHAPES[num_r%len(SHAPES)], 0, height+4)
+        # drop new rock so that its lowest point
+        # is 3 cells above the tower's highest point
+        height = highest_point(tower)
+        considered_rock = move(SHAPES[num_r%len(SHAPES)], 0, height+4)
         while True:
-            previous = current_shape
-            hot_move = -1 + 2*(hot_air[num_h%len(hot_air)] == '>')
+            last_possible_rock = considered_rock
+            dx = -1 + 2*(hot_air[num_h%len(hot_air)] == '>')
             num_h += 1
-            current_shape = update_position(current_shape, hot_move, 0)
+            considered_rock = move(considered_rock, dx, 0)
             
-            if any(x < 0 or x > 6 for (x, y) in current_shape) or \
-                touched(current_shape, taken):
-                current_shape = previous
-            previous = current_shape
-            current_shape = update_position(current_shape, 0, -1)
-            if touched(current_shape, taken):
-                taken |= set(previous)
+            # skip horizontal action if it would result with penetration
+            if penetrating_wall(considered_rock) or penetrating_floor(considered_rock, tower):
+                considered_rock = last_possible_rock
+            last_possible_rock = considered_rock
+            considered_rock = move(considered_rock, 0, -1)
+            if penetrating_floor(considered_rock, tower):
+                tower |= set(last_possible_rock)
                 break
 
-    return highest_point(taken)
+    return highest_point(tower)
 
 
 
 
 data = get_input('inputs/17.txt')[0]
 
-party_1 = play_tetris(2022, data)
+party_1 = drop_rocks(2022, data)
 print_solutions(party_1)
 
 
