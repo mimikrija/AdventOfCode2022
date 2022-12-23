e2d9dcfcd130be79cdf80e53584b0086439b0e38
Maja Gacesa <maja.gacesa@gmail.com>
2022-12-23 06:52:53 +0100

    [day23] I know what the bug is

--- python/23.py
+++ python/23.py
@@ -17,36 +17,36 @@ directions = deque([
 def play_round(elves, directions):
     proposals = defaultdict(list)
     for elf in elves:
-        print('or here')
         for direction in directions:
             if not any((candidate := elf + dir) in elves for dir in direction):
                 proposals[candidate].append(elf)
-                print('breaking..')
                 break
-        print('am I here?')
     for new_elf, old_elves in proposals.items():
         if len(old_elves) == 1:
+            # i need to add here that it does not move towards proposal but towards general direction
+            
             elves.remove(old_elves[0])
             elves.add(new_elf)
+    return elves
     if len(proposals) == 0: # no possible moves
         return elves
 
 def get_elves_count_rectangle(elves):
-    min_x = min(elves, key=lambda x: x.real).real
-    max_x = max(elves, key=lambda x: x.real).real
-    min_y = min(elves, key=lambda x: x.imag).imag
-    max_y = max(elves, key=lambda x: x.imag).imag
+    min_x = int(min(elves, key=lambda x: x.real).real)
+    max_x = int(max(elves, key=lambda x: x.real).real)
+    min_y = int(min(elves, key=lambda x: x.imag).imag)
+    max_y = int(max(elves, key=lambda x: x.imag).imag)
+    print(min_x, max_x, min_y, max_x)
     count = 0
     for x in range(min_x, max_x+1):
         for y in range(min_y, max_y+1):
-            if complex(x,y) in elves:
+            if complex(x,y) not in elves:
                 count += 1
     return count
 
 
 for _ in range(10):
     elves = play_round(elves, directions)
-    print(len(elves))
     directions.rotate(-1)
 
 print(get_elves_count_rectangle(elves))
